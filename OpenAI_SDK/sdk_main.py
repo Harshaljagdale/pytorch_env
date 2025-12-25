4ff041577ce0d3614f9cc339d7468e20870bac13
Harshal Jagdale <harshal.jagdale@gmail.com>
2025-12-24 17:40:21 -0800

    Dec2005

---
+++ OpenAI_SDK/sdk_main.py
@@ -0,0 +1,13 @@
+from agents import Agent, Runner
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+
+result = Runner.run_sync(agent, "Why sky is blue")
+print(result.final_output)
+
+# Code within the code,
+# Functions calling themselves,
+# Infinite loop's dance.
